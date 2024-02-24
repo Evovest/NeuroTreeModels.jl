39f68735aa04d07768ee4ade8529415819d6822c
Documenter.jl <documenter@juliadocs.github.io>
2024-02-24 06:54:55 +0000

    build based on a9d0c12

--- dev/assets/API.md.CM7CgeBt.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as i,c as e,o as s,a7 as a}from"./chunks/framework.BResyDFY.js";const E=JSON.parse('{"title":"API","description":"","frontmatter":{},"headers":[],"relativePath":"API.md","filePath":"API.md","lastUpdated":null}'),n={name:"API.md"},l=a("",7),t=[l];function r(o,p,d,h,k,c){return s(),e("div",null,t)}const u=i(n,[["render",r]]);export{E as __pageData,u as default};
