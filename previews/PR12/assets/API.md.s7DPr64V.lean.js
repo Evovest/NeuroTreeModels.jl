037f3b2408e5a02f03c8c318effc02cbc44e1819
Documenter.jl <documenter@juliadocs.github.io>
2024-04-21 17:47:17 +0000

    build based on 064585a

---
+++ previews/PR12/assets/API.md.s7DPr64V.lean.js
@@ -0,0 +1,1 @@
+import{_ as i,c as s,o as e,a7 as a}from"./chunks/framework.Clewx64d.js";const E=JSON.parse('{"title":"API","description":"","frontmatter":{},"headers":[],"relativePath":"API.md","filePath":"API.md","lastUpdated":null}'),n={name:"API.md"},t=a("",7),l=[t];function p(r,o,h,d,c,k){return e(),s("div",null,l)}const u=i(n,[["render",p]]);export{E as __pageData,u as default};
